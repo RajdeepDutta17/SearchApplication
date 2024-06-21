ac8bed7d86114598279c633a7de9d96dda13c3f4
RajdeepDutta17 <dutta10rajdeep@gmail.com>
2024-06-21 20:36:33 +0530

    added the deployment link in the readme file

--- src/App.jsx
+++ src/App.jsx
@@ -17,12 +17,12 @@ function App() {
     try {
       const res = await axios({
         method: "GET",
-        url: inputData
-          ? "https://search-app-api-qicn.onrender.com/api/v1/getEmployeeByQuery?searchQuery=" +
-            inputData
-          : "https://search-app-api-qicn.onrender.com/api/v1/getEmployee",
-        // "http://localhost:8080/api/v1/getEmployeeByQuery?searchQuery=" +
-        // inputData,
+        url:
+          //   ? "https://search-app-api-qicn.onrender.com/api/v1/getEmployeeByQuery?searchQuery=" +
+          //     inputData
+          //   : "https://search-app-api-qicn.onrender.com/api/v1/getEmployee",
+          "http://localhost:8080/api/v1/getEmployeeByQuery?searchQuery=" +
+          inputData,
       });
 
       setData(res.data.data);
@@ -92,6 +92,35 @@ function App() {
           </tfoot>
         </table>
       </div>
+      <nav aria-label="Page navigation example">
+        <ul className="pagination">
+          <li className="page-item">
+            <a className="page-link" href="#" aria-label="Previous">
+              <span aria-hidden="true">&laquo;</span>
+            </a>
+          </li>
+          <li className="page-item">
+            <a className="page-link" href="#">
+              1
+            </a>
+          </li>
+          <li className="page-item">
+            <a className="page-link" href="#">
+              2
+            </a>
+          </li>
+          <li className="page-item">
+            <a className="page-link" href="#">
+              3
+            </a>
+          </li>
+          <li className="page-item">
+            <a className="page-link" href="#" aria-label="Next">
+              <span aria-hidden="true">&raquo;</span>
+            </a>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 }
